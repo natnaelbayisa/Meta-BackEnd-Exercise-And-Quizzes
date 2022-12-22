a1bbef98101edfe973d127e66b6e6bf87b3f4f62
natnaelbayisa <natnaelbebe@gmail.com>
2022-12-22 17:25:03 +0300

    using mysql instead of sqllite

--- 5. django-web-framework/exercise/week3/ex3/myproject/myproject/settings.py
+++ 5. django-web-framework/exercise/week3/ex3/myproject/myproject/settings.py
@@ -76,13 +76,30 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+# sqllite database
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# mysql
+DATABASES = {   
+    'default': {   
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'testingDjango',   
+        'USER': 'root',   
+        'PASSWORD': 'Natimysql1//',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',   
+        'OPTIONS': {   
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+        }   
+    }   
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
