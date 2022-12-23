c413fa291d5adf0456fcc9ff369880c18c9fa61b
natnaelbayisa <natnaelbebe@gmail.com>
2022-12-23 13:41:31 +0300

    updated

--- 5. django-web-framework/exercise/week4/ex1/myproject/myapp/views.py
+++ 5. django-web-framework/exercise/week4/ex1/myproject/myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse 
+def about(request): 
+    about_content = {'about': "History of Lorem Ipsum originates with Cicero in the 1st Century BC and his text De Finibus bonorum et malorum. This philosophical work, also known as On the Ends of Good and Evil, was split into five books."}
+    return render(request, "about.html",  about_content) 
