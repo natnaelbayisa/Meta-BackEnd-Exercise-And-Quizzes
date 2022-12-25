5c7eb208cdaa12d4a519ab567d9953d2a1eb2acb
natnaelbayisa <natnaelbebe@gmail.com>
2022-12-25 12:56:42 +0300

    final project

--- 5. django-web-framework/exercise/week5/littlelemon/littlelemon/settings.py
+++ 5. django-web-framework/exercise/week5/littlelemon/littlelemon/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# render static files
+STATICFILES_DIRS = [
+    'restaurant/static',
+]
--- 5. django-web-framework/exercise/week5/littlelemon/restaurant/urls.py
+++ 5. django-web-framework/exercise/week5/littlelemon/restaurant/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.home),
-    path("about/", views.about),
-    path("menu/", views.menu),
-    path("book/", views.book)
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('book/', views.book, name="book"), 
+    path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),
 ]
