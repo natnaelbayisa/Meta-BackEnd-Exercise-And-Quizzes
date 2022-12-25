81b40c65b73cec06b368470c9588c687e18e93fe
natnaelbayisa <natnaelbebe@gmail.com>
2022-12-25 09:37:58 +0300

    updated

--- 5. django-web-framework/exercise/week4/ex2/myproject/myapp/models.py
+++ 5. django-web-framework/exercise/week4/ex2/myproject/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    booking_time = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.first_name
