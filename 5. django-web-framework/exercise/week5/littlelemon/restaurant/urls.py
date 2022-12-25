b3291b659b13da245534d131d09c217b3ffa955c
natnaelbayisa <natnaelbebe@gmail.com>
2022-12-25 10:29:39 +0300

    final project

---
+++ 5. django-web-framework/exercise/week5/littlelemon/restaurant/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home),
+    path("about/", views.about),
+    path("menu/", views.menu),
+    path("book/", views.book)
+]
